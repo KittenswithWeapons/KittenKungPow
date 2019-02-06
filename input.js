ae2d18747726a5027dcce074678383de4a562078
ljenny <ljenny@uw.edu>
2019-02-06 01:52:38 -0800

    incorporated new features

--- input.js
+++ input.js
@@ -31,14 +31,16 @@ function setupKeyboard(entity) {
     
     input.addMapping('KeyE', keyState => { //Fireball
         if (keyState) {
-          if (!entity.Throwing) {
-          entity.Throwing = true;
-          entity.updateAnimation();
-          ThrowProjectile("fireball", entity);
-        } else {entity.updateAnimation(); console.log('double try');}
-        } else {
-          entity.Throwing = false;
-          entity.updateAnimation();
+            if (!entity.Throwing) {
+                entity.Throwing = true;
+                entity.updateAnimation();
+                
+                window.setTimeout(function() {
+                    ThrowProjectile("fireball", entity);
+                    entity.Throwing = false;
+                    entity.updateAnimation();}, 280) //waits for the animation to be done, then throws and updates values
+
+            } else {entity.updateAnimation(); console.log('double try');}
         }
     });
 
