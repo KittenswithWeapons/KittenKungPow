445d6c5f8b76119058faff1239b52fb4ce2e412f
ljenny <ljenny@uw.edu>
2019-02-20 17:19:37 -0800

    fixed map size and improved ability cooldowns

--- keyboardState.js
+++ keyboardState.js
@@ -5,8 +5,7 @@ class keyBoardState {
 	constructor() {
 		this.keyStates = new Map(); //holds the current state of a key
 		this.keyMap = new Map(); //holds the callback functions for a code
-		
-	
+		this.frozen = false;
 	}
 	
 
@@ -16,27 +15,26 @@ class keyBoardState {
 	
 
 	handleEvent(event) {
-		
-		const {code} = event;
-		
-		
-		
-		if (!this.keyMap.has(code)){
-			return;
-		}
-		
-		event.preventDefault();
-		
-		const keyState = event.type === 'keydown' ? PRESSED:RELEASED;
-		
-		if(this.keyStates.get(code) === keyState) {
-			return;
-		}
-		
-		this.keyStates.set(code, keyState);
-		//console.log(this.keyStates);
-		
-		this.keyMap.get(code)(keyState);
+			const {code} = event;
+			
+			
+			
+			if (!this.keyMap.has(code)){
+				return;
+			}
+			
+			event.preventDefault();
+			
+			const keyState = event.type === 'keydown' ? PRESSED:RELEASED;
+			
+			if(this.keyStates.get(code) === keyState) {
+				return;
+			}
+			
+			this.keyStates.set(code, keyState);
+			//console.log(this.keyStates);
+			
+			this.keyMap.get(code)(keyState);
 	}
 	
 	
