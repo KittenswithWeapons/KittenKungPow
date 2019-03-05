528f472aaf48bd8825a6984091c4c2c0e97c7660
JOwens14 <jowens@uw.edu>
2019-03-04 19:43:47 -0800

    tile updates

--- sprites.js
+++ sprites.js
@@ -1,15 +1,15 @@
 function loadBackgroundSprites(name) {
-    return loadImage(`./Enviroment/${name}tiles.png`)
+    return loadImage('./Enviroment/tiles.png')
     .then(image => {
         if (name === 'FutureTown') {
           const sprites = new SpriteSheet(image, 32, 32);
-          sprites.defineTile('ground', 1, 0);
-          sprites.defineTile('platform', 0, 0);
+          sprites.defineTile('ground', 0, 2);
+          sprites.defineTile('platform', 8, 0);
           return sprites;
         } else if (name === 'PinkCity') {
           const sprites = new SpriteSheet(image, 32, 32);
-          sprites.defineTile('ground', 1, 0);
-          sprites.defineTile('platform', 1, 0);
+          sprites.defineTile('ground', 7, 0);
+          sprites.defineTile('platform', 7, 0);
           return sprites;
         } else if (name === 'FutureCity') {
           const sprites = new SpriteSheet(image, 32, 32);
@@ -18,28 +18,28 @@ function loadBackgroundSprites(name) {
           return sprites;
         } else if (name === 'Waterfall') {
           const sprites = new SpriteSheet(image, 32, 32);
-          sprites.defineTile('ground', 1, 0);
+          sprites.defineTile('ground', 0, 2);
           sprites.defineTile('platform', 0, 0);
           return sprites;
         } else if (name === 'LightMeows') {
           const sprites = new SpriteSheet(image, 32, 32);
-          sprites.defineTile('ground', 1, 0);
-          sprites.defineTile('platform', 1, 0);
+          sprites.defineTile('ground', 3, 0);
+          sprites.defineTile('platform', 3, 0);
           return sprites;
         } else if (name === 'BlueNight') {
           const sprites = new SpriteSheet(image, 32, 32);
-          sprites.defineTile('ground', 0, 0);
-          sprites.defineTile('platform', 0, 0);
+          sprites.defineTile('ground', 5, 0);
+          sprites.defineTile('platform', 5, 0);
           return sprites;
         } else if (name === 'Ocean') {
           const sprites = new SpriteSheet(image, 32, 32);
-          sprites.defineTile('ground', 0, 0);
-          sprites.defineTile('platform', 0, 0);
+          sprites.defineTile('ground', 1, 0);
+          sprites.defineTile('platform', 1, 0);
           return sprites;
         } else if (name === 'Dojo') {
           const sprites = new SpriteSheet(image, 32, 32);
-          sprites.defineTile('ground', 0, 0);
-          sprites.defineTile('platform', 0, 0);
+          sprites.defineTile('ground', 4, 0);
+          sprites.defineTile('platform', 4, 0);
           return sprites;
         }
     });
