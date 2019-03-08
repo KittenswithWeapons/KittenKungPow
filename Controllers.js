59c5cbc727fcee67b4e85e5528b740fb05a6ab6c
JOwens14 <44217575+JOwens14@users.noreply.github.com>
2019-03-07 19:58:30 -0800

    controller update

--- Controllers.js
+++ Controllers.js
@@ -5,6 +5,10 @@ var A_buttonPressed = false;
 var B_buttonPressed = false;
 var X_buttonPressed = false;
 var Y_buttonPressed = false;
+var right = false;
+var left = false;
+var idle = false;
+var up = false;
 
 function setUpControllers(entity) {
     window.addEventListener("gamepadconnected", function(e) {
@@ -38,59 +42,148 @@ function controllerUpdate(entity, controllerNUM){
 	if (controllers[controllerNUM] != null){   //checks for controller connected
 
     //BUTTON SECTION------------------------------------------------------------
-    
-    // A Button----------------------------
+
+    // A Button-----------------------------------------------------------------
 		if (controllers[controllerNUM].buttons[0].pressed){  // A button
 			if (controllers[controllerNUM].A_buttonPressed === false) {
 				controllers[controllerNUM].A_buttonPressed = true;
         // do A button stuff
-        entity.punch.start();
-        entity.Punching = true;
-        entity.updateAnimation();
+        if (!isPaused) {
+          entity.Jumping = true;
+          entity.grounded = false;
+          entity.jump.start();
+          entity.updateAnimation();
+        }
         //
 			}
 		} else {
 			controllers[controllerNUM].A_buttonPressed = false;
 		}
-    //------------------------------------
+    //--------------------------------------------------------------------------
 
-    // B Button---------------------------
+    // B Button-----------------------------------------------------------------
 		if (controllers[controllerNUM].buttons[1].pressed){  // B button???
 			if (controllers[controllerNUM].B_buttonPressed === false) {
 				controllers[controllerNUM].B_buttonPressed = true;
         // do B button stuff
-        if (!entity.Special) {
-          entity.Special = true;
+        if (!isPaused) {
+          entity.Heavy = true;
           entity.updateAnimation();
-          window.setTimeout(function() {
-          ThrowProjectile("fireball", entity);
-          entity.Special = false;
-          entity.updateAnimation();} , 280)
         }
         //
 			}
 		} else {
 			controllers[controllerNUM].B_buttonPressed = false;
 		}
+    //--------------------------------------------------------------------------
+
+    // X Button-----------------------------------------------------------------
+		if (controllers[controllerNUM].buttons[2].pressed){  // X button???
+			if (controllers[controllerNUM].X_buttonPressed === false) {
+				controllers[controllerNUM].X_buttonPressed = true;
+        // do X button stuff
+        if (!isPaused) {
+          entity.Light = true;
+          entity.updateAnimation();
+        }
+        //
+			}
+		} else {
+			controllers[controllerNUM].X_buttonPressed = false;
+		}
+    //--------------------------------------------------------------------------
+
+    // Y Button-----------------------------------------------------------------
+		if (controllers[controllerNUM].buttons[3].pressed){  // Y button???
+			if (controllers[controllerNUM].Y_buttonPressed === false) {
+				controllers[controllerNUM].Y_buttonPressed = true;
+        // do Y button stuff
+        if (!isPaused) {
+          if (!entity.Special) {
+            entity.Special = true;
+            entity.updateAnimation();
+          }
+        }
+        //
+			}
+		} else {
+			controllers[controllerNUM].Y_buttonPressed = false;
+		}
+    //--------------------------------------------------------------------------
 
 
-
+    //**************************************************************************
+    //**************************************************************************
+    //**************************************************************************
 
 
     //JOYSTICK SECTION----------------------------------------------------------
     //left right action
-		if (controllers[controllerNUM].axes[0] > 0.1) { //go right
-			entity.go.dir = 1;
+		if (controllers[controllerNUM].axes[0] > 0.3) { //go right
+      if (!right) {
+        if (!isPaused) {
+          entity.Walking = true;
+          entity.go.dir = 1;
+          entity.updateAnimation();
+          if(entity.dustFinished == true) {
+              entity.dustFinished = false;
+          }
+        }
+        idle = false;
+        right = true;
+      }
 			//console.log('RIGHT');
-		} else if (controllers[controllerNUM].axes[0] < -0.1) { //go left
-			entity.go.dir = -1;
+		} else if (controllers[controllerNUM].axes[0] < -0.3) { //go left
+        if (!left) {
+          if (!isPaused) {
+            entity.Walking = true;
+            entity.go.dir = -1;
+            entity.updateAnimation();
+            if(entity.dustFinished == true) {
+                entity.dustFinished = false;
+            }
+          }
+          idle = false;
+          left = true
+        }
 			//console.log('LEFT');
-		} else if (controllers[controllerNUM].axes[0] < 0.1 & controllers[controllerNUM].axes[0] > -0.1 ) { // stops the kitty
+		} else if (controllers[controllerNUM].axes[0] < 0.3 & controllers[controllerNUM].axes[0] > -0.3 ) { // stops the kitty
 			entity.go.dir = 0;
+      entity.Walking = false;
+      right = false;
+      left = false;
+      if (!idle) {
+        idle = true;
+        entity.updateAnimation();
+      }
 		}
     //--------------------------------------------------------------------------
 
     //jump and passdown JOYSTICK SECTION----------------------------------------
+
+    if (controllers[controllerNUM].axes[1] > 0.7) { //go down
+      if (!down) {
+        if (!isPaused) {
+          entity.passdown.start();
+        }
+      }
+		} else if (controllers[controllerNUM].axes[1] < -0.7) { //go up
+        if (!up) {
+          up = true;
+          if (!isPaused) {
+            entity.Jumping = true;
+            entity.grounded = false;
+            entity.jump.start();
+            entity.updateAnimation();
+          }
+        }
+		} else if (controllers[controllerNUM].axes[1] < 0.7 & controllers[controllerNUM].axes[1] > -0.7 ) { // stops the kitty
+			//do nothing
+      entity.passdown.cancel();
+      down = false;
+      up = false;
+		}
+
 
 
     //--------------------------------------------------------------------------
